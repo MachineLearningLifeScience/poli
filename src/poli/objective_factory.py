8f96f1662856333c8c131edaaca142fe753e98f3
Simon Bartels <bartels@di.ku.dk>
2023-01-04 20:45:54 +0100

    improved documentation

--- src/poli/objective_factory.py
+++ src/poli/objective_factory.py
@@ -3,11 +3,26 @@ This is the main file relevant for users who want to run objective functions.
 """
 import os
 import subprocess
+from typing import Callable
+
 import numpy as np
 from multiprocessing.connection import Listener
 
 
-def create(name: str, caller_info):
+def create(name: str, caller_info) -> (Callable[[np.ndarray], np.ndarray], np.ndarray, np.ndarray, str, Callable):
+    """
+    Instantiantes a black-box function.
+    :param name:
+        The name of the objective function or a shell-script for execution.
+    :param caller_info:
+        Optional information about the caller that is forwarded to the logger to initialize the run.
+    :return:
+        f: an objective function that accepts a numpy array and returns a numpy array
+        x0: initial inputs
+        y0: f(x0)
+        run_info: information from the logger about the instantiated run (allows the calling algorithm to connect)
+        terminate: a function to end the process behind f
+    """
     cwd = os.getcwd()
     if not name.endswith(".sh"):
         #cwd = os.path.dirname(__file__)  # execute script in project main folder
