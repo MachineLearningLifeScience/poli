5db4a12fad02df122f865d4fd2bfc14b5a354b60
Simon Bartels <bartels@di.ku.dk>
2023-01-04 17:11:24 +0100

    finished IPC

--- objective.py
+++ objective.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 import signal
 import configparser
+from multiprocessing.connection import Client
 
 from core.AbstractProblemFactory import AbstractProblemFactory
 from core.registry import INIT_DATA_FILE, INPUT_DATA_FILE, OUTPUT_DATA_FILE
@@ -17,42 +18,32 @@ def dynamically_instantiate(obj: str):
 
 
 if __name__ == '__main__':
-    signal.raise_signal(signal.SIGUSR1)
-    try:
-        config = configparser.ConfigParser()
-        config.read('config.rc')
-        # instantiate logger
-        logger: AbstractLogger = dynamically_instantiate(config['DEFAULT']['logger'])
+    config = configparser.ConfigParser()
+    config.read('config.rc')
+    # instantiate logger
+    logger: AbstractLogger = dynamically_instantiate(config['DEFAULT']['logger'])
 
-        # instantiate observer
-        observer: AbstractObserver = dynamically_instantiate(config['DEFAULT']['observer'])
-        observer.initialize(logger)
+    # instantiate observer
+    observer: AbstractObserver = dynamically_instantiate(config['DEFAULT']['observer'])
+    observer.initialize(logger)
 
-        # dynamically load objective function module
-        objective_name = sys.argv[1]
-        objective_factory: AbstractProblemFactory = dynamically_instantiate(objective_name)
+    # dynamically load objective function module
+    objective_name = sys.argv[1]
+    objective_factory: AbstractProblemFactory = dynamically_instantiate(objective_name)
 
-        # TODO: read caller_info
-        caller_info = {}
-        run_info = logger.initialize_logger(objective_factory.get_setup_information(), caller_info)
+    address = ('localhost', 6000)
+    conn = Client(address, authkey=b'secret password')
+    caller_info = conn.recv()
+    run_info = logger.initialize_logger(objective_factory.get_setup_information(), caller_info)
 
-        f, x0, y0 = objective_factory.create()
-        # add observer
-        f.set_observer(observer)
+    f, x0, y0 = objective_factory.create()
+    # add observer
+    f.set_observer(observer)
 
-        # tell mother-process that initial data is ready
-        np.save(INIT_DATA_FILE, np.hstack([x0, y0]))
-        signal.raise_signal(signal.SIG_UNBLOCK)
+    conn.send([x0, y0, run_info])
 
-        # now wait for objective function calls
-        while True:
-            sig = signal.sigwait([signal.SIG_BLOCK, signal.SIGQUIT])
-            if sig == signal.SIGQUIT:
-                break
-            x = np.load(INPUT_DATA_FILE)
-            y = f(x)
-            np.save(OUTPUT_DATA_FILE, y)
-            signal.raise_signal(signal.SIG_UNBLOCK)
-    except Exception as e:
-        signal.raise_signal(signal.SIGQUIT)
-        raise e
+    # now wait for objective function calls
+    while not conn.closed:
+        x = conn.recv()
+        y = f(x)
+        conn.send(y)
--- objective_factory.py
+++ objective_factory.py
@@ -5,6 +5,7 @@ import os
 import signal
 import subprocess
 import numpy as np
+from multiprocessing.connection import Listener
 
 from core.registry import INIT_DATA_FILE, INPUT_DATA_FILE, OUTPUT_DATA_FILE
 
@@ -12,40 +13,20 @@ from core.registry import INIT_DATA_FILE, INPUT_DATA_FILE, OUTPUT_DATA_FILE
 def create(name: str, caller_info):
     cwd = os.path.dirname(__file__)  # execute script in project main folder
     # write caller_info for logger in initialization
-    proc = subprocess.Popen("./objective_run_scripts/" + name + ".sh", stdout=None, stderr=None, cwd=cwd, shell=False)
+    proc = subprocess.Popen("./objective_run_scripts/" + name + ".sh", stdout=None, stderr=None, cwd=cwd)
 
-    # TODO: pass caller_info
-    print("caller info: " + str(caller_info))
-
-    _wait()
-    xy = np.load(INIT_DATA_FILE)
-    # TODO: delete file?
-    x0 = xy[:, :-1]
-    y0 = xy[:, -1:]
-
-    # TODO: load logger handle
-    run_info = None
+    address = ('localhost', 6000)
+    listener = Listener(address, authkey=b'secret password')
+    conn = listener.accept()
+    conn.send(caller_info)
+    x0, y0, run_info = conn.recv()
 
     def f(x: np.ndarray) -> np.ndarray:
-        # write x to destination
-        np.save(INPUT_DATA_FILE, x)
-        # signal process
-        proc.send_signal(signal.SIG_BLOCK)
-        _wait()
-
-        # read result
-        val = np.load(OUTPUT_DATA_FILE)
+        conn.send(x)
+        val = conn.recv()
         return val
 
     def terminate():
-        proc.send_signal(signal.CTRL_C_EVENT)
+        conn.close()
 
     return f, x0, y0, run_info, terminate
-
-
-def _wait():
-    sig = None
-    while sig is None:
-        sig = signal.sigtimedwait([signal.SIGQUIT, signal.SIG_UNBLOCK], 1)
-        if sig == signal.SIGQUIT:
-            raise RuntimeError("Unexpected exception in the objective function process.")
