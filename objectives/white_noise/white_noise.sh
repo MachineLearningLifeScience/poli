27a276cce9649bf14a692fa02d6fab8c4a5ab823
Simon Bartels <bartels@di.ku.dk>
2023-01-12 20:52:56 +0100

    adapted white noise factory to new location

--- objectives/white_noise/white_noise.sh
+++ objectives/white_noise/white_noise.sh
@@ -3,4 +3,5 @@ CONDA_BASE=$(conda info --base)
 source $CONDA_BASE/etc/profile.d/conda.sh
 conda activate /home/simon/stuff/projects/bayesian_optimization/prot_bo/env
 BASEDIR=$(dirname "$0")
-python $BASEDIR/../../objective.py poli.objectives.white_noise.white_noise_factory.WhiteNoiseFactory
+PYTHONPATH="${PYTHONPATH}:/${BASEDIR}"
+python $BASEDIR/../../src/poli/objective.py white_noise_factory.WhiteNoiseFactory
--- objectives/white_noise/white_noise_factory.py
+++ objectives/white_noise/white_noise_factory.py
@@ -1,7 +1,6 @@
 __author__ = 'Simon Bartels'
 
 import os.path
-
 import numpy as np
 
 import poli.core.registry
@@ -22,15 +21,16 @@ class WhiteNoiseFactory(AbstractProblemFactory):
         return ProblemSetupInformation(name="WHITE_NOISE", max_sequence_length=5, aligned=True, alphabet=AA_IDX)
 
     def create(self) -> (AbstractBlackBox, np.ndarray, np.ndarray):
-        class WhiteNoiseBlackBox(AbstractBlackBox):
-            def _black_box(self, x: np.ndarray) -> np.ndarray:
-                # the returned value must be a [1, 1] array
-                return np.random.randn(1).reshape(-1, 1)
-
         f = WhiteNoiseBlackBox(self.get_setup_information().get_max_sequence_length())
         x = np.diag(np.arange(11) + 6) @ np.ones([11, self.get_setup_information().get_max_sequence_length()], dtype=np.int)
         y = f(x)
         return f, x, y
+
+
+class WhiteNoiseBlackBox(AbstractBlackBox):
+    def _black_box(self, x: np.ndarray, context=None) -> np.ndarray:
+        # the returned value must be a [1, 1] array
+        return np.random.randn(1).reshape(-1, 1)
 
 
 if __name__ == '__main__':
