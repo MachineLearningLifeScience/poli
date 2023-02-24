a0bad65df5de83e0f8032c69ac8305279cf2fa54
Simon Bartels <bartels@di.ku.dk>
2023-02-24 13:17:29 +0100

    moved lambo objectives to new project

--- objectives/white_noise/white_noise.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-CONDA_BASE=$(conda info --base)
-source $CONDA_BASE/etc/profile.d/conda.sh
-conda activate /home/simon/stuff/projects/bayesian_optimization/prot_bo/env
-BASEDIR=$(dirname "$0")
-PYTHONPATH="${PYTHONPATH}:/${BASEDIR}"
-python $BASEDIR/../../src/poli/objective.py white_noise_factory.WhiteNoiseFactory
