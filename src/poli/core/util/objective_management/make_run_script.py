7202fdf68e6cc2cde40e0ed5f8db244720ba29ed
Simon Bartels <bartels@di.ku.dk>
2023-07-07 11:52:54 +0200

    found a way to find the conda environment if not specified

--- src/poli/core/util/objective_management/make_run_script.py
+++ src/poli/core/util/objective_management/make_run_script.py
@@ -1,6 +1,7 @@
 __author__ = 'Simon Bartels'
 
 import os
+import sys
 from os.path import basename, dirname, join
 import inspect
 import stat
@@ -43,25 +44,24 @@ def _make_run_script(command, instantiated_object, conda_environment_location, p
     package_name += "."
     full_problem_factory_name = package_name + problem_factory_name
     run_script_location = join(RUN_SCRIPTS_FOLDER, problem_factory_name + ".sh")
-    if conda_environment_location is not None:
-        # make path to conda environment absolute
-        conda_environment_location = str(os.path.abspath(conda_environment_location))
-        if python_paths is None:
-            python_paths = [dirname(factory_location)]
-        # TODO: check that location exists and is valid environment
-        python_paths = ":".join(python_paths)
-        with open(join(dirname(__file__), "run_script_template.sht"), "r") as run_script_template_file:
-            """
-            load template file and fill in gaps which load conda enviroment, append python paths and call run with the 
-            desired factory
-            """
-            run_script = run_script_template_file.read() % (cwd, conda_environment_location, python_paths,
-                                                            ADDITIONAL_IMPORT_SEARCH_PATHES_KEY,
-                                                            command + " " + full_problem_factory_name)
-        with open(run_script_location, "w+") as run_script_file:
-            # write out run script and make it executable
-            run_script_file.write(run_script)
-            os.chmod(run_script_location, os.stat(run_script_location).st_mode | stat.S_IEXEC)  # make script file executable
-    else:
-        raise NotImplementedError("Currently a conda environment is required.")
+    if conda_environment_location is None:
+        conda_environment_location = os.path.join(*sys.executable.split(os.path.sep)[:-2])
+    # make path to conda environment absolute
+    conda_environment_location = str(os.path.abspath(conda_environment_location))
+    if python_paths is None:
+        python_paths = [dirname(factory_location)]
+    # TODO: check that location exists and is valid environment
+    python_paths = ":".join(python_paths)
+    with open(join(dirname(__file__), "run_script_template.sht"), "r") as run_script_template_file:
+        """
+        load template file and fill in gaps which load conda enviroment, append python paths and call run with the 
+        desired factory
+        """
+        run_script = run_script_template_file.read() % (cwd, conda_environment_location, python_paths,
+                                                        ADDITIONAL_IMPORT_SEARCH_PATHES_KEY,
+                                                        command + " " + full_problem_factory_name)
+    with open(run_script_location, "w+") as run_script_file:
+        # write out run script and make it executable
+        run_script_file.write(run_script)
+        os.chmod(run_script_location, os.stat(run_script_location).st_mode | stat.S_IEXEC)  # make script file executable
     return run_script_location
