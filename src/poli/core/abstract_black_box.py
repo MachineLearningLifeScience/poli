fb5f009a478dd43b4d94deab948a7ce980352cca
Simon Bartels <bartels@di.ku.dk>
2023-08-22 18:42:44 +0200

    fixed an error popping up in batch evaluations

--- src/poli/core/abstract_black_box.py
+++ src/poli/core/abstract_black_box.py
@@ -22,7 +22,7 @@ class AbstractBlackBox:
         """
         x_ = x[:1, ...]
         # TODO: activate assertion below for discrete problems
-        #assert(all([c in self.info.get_alphabet() for c in x_]))
+        assert(all([c in self.info.get_alphabet() for c in x_[0]]))
         f_ = self._black_box(x_, context)
         assert (isinstance(f_, np.ndarray))
         assert (len(f_.shape) == 2)
@@ -35,7 +35,7 @@ class AbstractBlackBox:
         f[:1, ...] = f_
         for i in range(1, x.shape[0]):
             x_ = x[i:i+1, ...]
-            assert (all([c in self.info.get_alphabet() for c in x_]))
+            assert (all([c in self.info.get_alphabet() for c in x_[0]]))
             f_ = self._black_box(x_, context)
             f[i:i+1, ...] = f_
             if self.observer is not None:
