2eaf87558d73b03f71cdf5b3f0659542fddb48f2
Richard Michael <r.michael@posteo.net>
2023-10-03 16:03:24 +0200

    disable iterative calls, if terative required: call _black_box in loop

--- src/poli/core/abstract_black_box.py
+++ src/poli/core/abstract_black_box.py
@@ -87,13 +87,7 @@ class AbstractBlackBox:
                     )
                     f_batch = np.array(f_batch_).reshape(len(x_batch_), -1)
             else:  # iterative treatment
-                if x_batch.shape[0] > 1:
-                    _f_batch = np.array(
-                        [self._black_box(_x, context) for _x in x_batch]
-                    )
-                    f_batch = np.array(_f_batch).reshape(len(x_batch_), -1)
-                else:
-                    f_batch = self._black_box(x_batch, context)
+                f_batch = self._black_box(x_batch, context)
 
             assert (
                 len(f_batch.shape) == 2
