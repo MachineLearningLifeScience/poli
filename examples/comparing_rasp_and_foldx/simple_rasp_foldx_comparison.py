b3e8694ece66e213d2bba5e7c2ad827b72393345
Miguel González Duque <miguelgondu@gmail.com>
2023-10-25 17:52:12 +0200

    Uncomments foldx example and lints

--- examples/comparing_rasp_and_foldx/simple_rasp_foldx_comparison.py
+++ examples/comparing_rasp_and_foldx/simple_rasp_foldx_comparison.py
@@ -10,19 +10,25 @@ from poli import objective_factory
 THIS_DIR = Path(__file__).parent.resolve()
 
 if __name__ == "__main__":
-    wildtype_pdb_paths_for_foldx = list((THIS_DIR / "example_pdbs").glob("*_Repair.pdb"))
+    wildtype_pdb_paths_for_foldx = list(
+        (THIS_DIR / "example_pdbs").glob("*_Repair.pdb")
+    )
     wildtype_pdb_paths_for_rasp = list((THIS_DIR / "example_pdbs").glob("*.pdb"))
     wildtype_pdb_paths_for_rasp = [
-        path_ for path_ in wildtype_pdb_paths_for_rasp if "_Repair" not in str(path_.name)
+        path_
+        for path_ in wildtype_pdb_paths_for_rasp
+        if "_Repair" not in str(path_.name)
     ]
 
-    # _, f_foldx, x0, y0, _ = objective_factory.create(
-    #     name="foldx_stability",
-    #     wildtype_pdb_path=wildtype_pdb_paths,
-    #     batch_size=1,
-    # )
+    _, f_foldx, x0, y0, _ = objective_factory.create(
+        name="foldx_stability",
+        wildtype_pdb_path=wildtype_pdb_paths_for_foldx,
+        batch_size=1,
+    )
 
-    # print(f_foldx(x0))
+    print(f_foldx(x0))
+
+    f_foldx.terminate()
 
     _, f_rasp, x0, y0, _ = objective_factory.create(
         name="rasp",
