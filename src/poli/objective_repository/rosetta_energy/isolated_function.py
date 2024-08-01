c915c1331879868bb848f6bd2d38bdeb20415586
Miguel González Duque <miguelgondu@gmail.com>
2024-08-01 09:25:42 +0200

    Starts playing around with PyRosetta

--- src/poli/objective_repository/rosetta_energy/isolated_function.py
+++ src/poli/objective_repository/rosetta_energy/isolated_function.py
@@ -1,17 +1,15 @@
-import os
+from pathlib import Path
 
 import numpy as np
 import pyrosetta
 from pyrosetta.rosetta.protocols.loops import get_fa_scorefxn
 from pyrosetta.rosetta.protocols.minimization_packing import MinMover
-from pyrosetta.rosetta.core.kinematics import MoveMap
-from poli.core.abstract_isolated_function import AbstractIsolatedFunction
-from poli.objective_repository.rosetta_energy import rosetta_energy_information
 
+from poli.core.abstract_isolated_function import AbstractIsolatedFunction
 
 pyrosetta.init()
 
-ROSETTA_ENERGY_DIR = os.path.dirname(__file__)
+ROSETTA_ENERGY_DIR = Path(__file__).parent.resolve()
 
 
 class RosettaEnergyIsolatedLogic(AbstractIsolatedFunction):
@@ -20,9 +18,8 @@ class RosettaEnergyIsolatedLogic(AbstractIsolatedFunction):
         self.energy_function = get_fa_scorefxn()
         self.mover.score_function(self.energy_function)
 
-
     def __call__(self, x: np.ndarray, context=None) -> np.ndarray:
-        #protein = pyrosetta.pose_from_pdb(ROSETTA_ENERGY_DIR + "/1ggx.pdb")
+        # protein = pyrosetta.pose_from_pdb(ROSETTA_ENERGY_DIR + "/1ggx.pdb")
         y = np.empty([x.shape[0], 1])
         for i in range(x.shape[0]):
             seq = "".join(x[i, :].tolist())
@@ -36,10 +33,22 @@ class RosettaEnergyIsolatedLogic(AbstractIsolatedFunction):
 
 
 if __name__ == "__main__":
-    from poli.core.registry import register_isolated_function
+    # from poli.core.registry import register_isolated_function
 
-    register_isolated_function(
-        RosettaEnergyIsolatedLogic,  # Your function, designed to be isolated
-        name=rosetta_energy_information.get_problem_name() + "__isolated",  #  Same name as the problem and folder, ending on __isolated.
-        conda_environment_name="poli__rosetta_energy",  # The name of the conda env inside environment.yml.
-    )
+    # register_isolated_function(
+    #     RosettaEnergyIsolatedLogic,  # Your function, designed to be isolated
+    #     name=rosetta_energy_information.get_problem_name() + "__isolated",  #  Same name as the problem and folder, ending on __isolated.
+    #     conda_environment_name="poli__rosetta_energy",  # The name of the conda env inside environment.yml.
+    # )
+    isolated_logic = RosettaEnergyIsolatedLogic()
+    from poli.core.util.proteins.pdb_parsing import parse_pdb_as_residue_strings
+
+    pdb_path = Path(ROSETTA_ENERGY_DIR) / "1ggx.pdb"
+    seq = parse_pdb_as_residue_strings(pdb_path)
+
+    print(seq)
+    y = isolated_logic._apply_on_sequence("".join(seq))
+    print(y)
+
+    pose = pyrosetta.pose_from_pdb(str(ROSETTA_ENERGY_DIR / "1ggx.pdb"))
+    print(pose)
