40a94c67a905570ba2b54ad2dcd4ba418fc4e1c1
Miguel González Duque <miguelgondu@gmail.com>
2024-08-08 15:23:04 +0200

    Adds the character tokenizer

---
+++ src/poli/core/util/tokenizers/character_tokenizer.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from poli.core.util.tokenizers.abstract_tokenizer import AbstractTokenizer
+
+
+class CharacterTokenizer(AbstractTokenizer):
+    def _tokenize(self, texts: str | list[str]) -> list[str] | list[list[str]]:
+        if isinstance(texts, str):
+            return list(texts)
+        elif isinstance(texts, list):
+            return [list(t) for t in texts]
+        else:
+            raise ValueError(f"Expected str or list, got {type(texts)}")
