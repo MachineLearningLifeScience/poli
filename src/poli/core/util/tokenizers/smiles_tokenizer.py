a9069fc0d1099406a9fda7a7c41c1788b7bbab11
Miguel González Duque <miguelgondu@gmail.com>
2024-08-08 18:46:48 +0200

    Returns to the previous tokenizer

--- src/poli/core/util/tokenizers/smiles_tokenizer.py
+++ src/poli/core/util/tokenizers/smiles_tokenizer.py
@@ -9,10 +9,12 @@ class SMILESTokenizer(AbstractTokenizer):
     def __init__(
         self, max_sequence_length: int | float | None = None, padding_token: str = ""
     ) -> None:
-        # DeepChem's SMILES tokenizer plus tokenizing single integers
-        self.REGEX_FOR_SMILES = re.compile(
-            r"(\[[^\]]+]|Br?|Cl?|N|O|S|P|F|I|b|c|n|o|s|p|\(|\)|\.|=|#||\+|\\\\\/|:||@|\?|>|\*|\$|\%[0-9]{2}|[0-9]|\d{1})"
-        )
+        """Tokenizer for SMILES strings, based on DeepChem's basic SMILES tokenizer."""
+        # DeepChem's SMILES tokenizer
+        SMI_REGEX_PATTERN = r"""(\[[^\]]+]|Br?|Cl?|N|O|S|P|F|I|b|c|n|o|s|p|\(|\)|\.|=|
+#|-|\+|\\|\/|:|~|@|\?|>>?|\*|\$|\%[0-9]{2}|[0-9])"""
+
+        self.REGEX_FOR_SMILES = re.compile(SMI_REGEX_PATTERN)
         super().__init__(max_sequence_length, padding_token)
 
     def _tokenize(self, texts: str | list[str]) -> list[str] | list[list[str]]:
