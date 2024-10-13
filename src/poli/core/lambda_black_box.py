696ce42e0eb0e26fbcbe459dd53f63d5f37a4bbc
Miguel González Duque <miguelgondu@gmail.com>
2024-10-13 18:53:34 +0200

    Adds documentation

--- src/poli/core/lambda_black_box.py
+++ src/poli/core/lambda_black_box.py
@@ -4,6 +4,11 @@ This module implements a lambda black box:
 Once the user has specified a callable f which
 takes as input np.ndarray and returns np.ndarray,
 this black box uses it for the evaluation.
+
+This is useful when the user has already implemented
+a lot of functionality around a callable, and wants to
+leverage all that comes with a poli black box: logging,
+budget management, etc.
 """
 
 from __future__ import annotations
@@ -17,6 +22,33 @@ from poli.core.black_box_information import BlackBoxInformation
 
 
 class LambdaBlackBox(AbstractBlackBox):
+    """
+    A Lambda black box is a black box that uses a callable
+    for the evaluation.
+
+    Parameters:
+    -----------
+    function : Callable[[np.ndarray], np.ndarray]
+        The callable that will be used for the evaluation.
+    info : BlackBoxInformation, optional
+        The information about the black box. If None,
+        a default information is used.
+    batch_size : int, optional
+        The batch size for evaluating the black box function. Default is None.
+    parallelize : bool, optional
+        Flag indicating whether to evaluate the black box function in parallel.
+        Default is False.
+    num_workers : int, optional
+        The number of workers to use for parallel evaluation. Default is None,
+        which uses half of the available CPU cores.
+    evaluation_budget : int, optional
+        The maximum number of evaluations allowed for the black box function.
+        Default is float("inf").
+    force_isolation : bool, optional
+        Flag indicating whether to force isolation of the black
+        box function. In this black box, this flag is ignored.
+    """
+
     def __init__(
         self,
         function: Callable[[np.ndarray], np.ndarray],
