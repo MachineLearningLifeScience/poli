83e50f0898998fcbf5538d997cf70c6b1e72b382
Richard Michael <r.michael@posteo.net>
2024-10-22 11:01:33 +0200

    add docstrings, add constraint weight to factory create and BB init

--- src/poli/objective_repository/rosetta_energy/register.py
+++ src/poli/objective_repository/rosetta_energy/register.py
@@ -1,3 +1,20 @@
+"""
+This module implements Rosetta-energy Stability Predictions,
+using PyRosetta-4 [1].
+
+Rosetta is a black box that starts
+with one wildtype pdb file, and assesses the
+stability of a variant.
+Two general running-modes are available: 
+i) fast prediction with MinMover and scoring function -- set relax=False, pack=False
+ii) default protocol prediction with pack and relax algorithm -- default .
+
+[1] Chaudhury, Sidhartha, Sergey Lyskov, and Jeffrey J. Gray. 
+"PyRosetta: a script-based interface for implementing molecular modeling algorithms using Rosetta." 
+Bioinformatics 26.5 (2010): 689-691. https://doi.org/10.1093/bioinformatics/btq007 
+
+"""
+
 from __future__ import annotations
 
 from pathlib import Path
@@ -47,6 +64,66 @@ def opt_in_wrapper(f: Callable, *args, **kwargs):
 
 
 class RosettaEnergyBlackBox(AbstractBlackBox):
+    """
+    RosettaEnergy Black Box implementation.
+
+    Parameters
+    ----------
+    wildtype_pdb_path : Path
+        The path to the wildtype PDB file(s), by default None.
+        NOTE: currently only for single PDB files implemented -- List of Paths not yet supported!
+    score_function : str, optional
+        Which Rosetta score function to use. Options are [ref2015 , default , centroid , fa, ref2015_cart, franklin2019].
+        The default function references ref2015.
+    seed : int, optional
+        Overwrite Rosetta random seed with own integer, uses mt19937 RT reference (as per Rosetta default).
+    unit : str, optional
+        Output unit of black-box. Default is DDG, which is scaled difference between variant and wild-type.
+        Alternatives are:
+            REU -- raw energy function value,
+            DREU -- energy unit delta to wild-type.
+    conversion_factor : float, optional
+        Scaling factor applied when computing DDGs (DDG=deltaREU/conversion_factor). Only applies to DDG conversion.
+        Default is '2.9' following:
+            Park, Hahnbeom, et al.
+            "Simultaneous optimization of biomolecular energy functions on features from small molecules and macromolecules."
+            Journal of chemical theory and computation 12.12 (2016): 6201-6212.
+            DOI: https://doi.org/10.1021/acs.jctc.6b00819
+    clean : bool, optional
+        Flag whether to apply PDB cleaning routine prior to pose loading of reference.
+        Default is True.
+    relax: bool, optional
+        Flag whether to apply relaxation protocol. Default enabled.
+        NOTE: disable if fast compute required
+    pack: bool, optional
+        Flag whether to apply packing protocol. Default enabled.
+        NOTE: disable if fast compute required
+    cycle: int, optional
+        Number of relaxation cycles applied.
+    constraint_weight: int, optional
+        Constraint on scoring function for atom-pair, angles, coordinates.
+
+    Methods
+    -------
+    _black_box(x, context=None)
+        The main black box method that performs the computation, i.e.
+        it computes the stability of the mutant(s) in x.
+    get_black_box_info()
+        Returns BlackBox info object.
+
+    Raises
+    ------
+    AssertionError
+        If wildtype_pdb_path is not provided.
+        If the provided energy units are not valid.
+    NotImplementedError
+        If energy function does not exist.
+
+    Notes
+    -----
+    - The wildtype_pdb_path is a single Path object.
+    """
+
     def __init__(
         self,
         wildtype_pdb_path: Path | List[Path],
@@ -58,6 +135,7 @@ class RosettaEnergyBlackBox(AbstractBlackBox):
         relax: bool = True,
         pack: bool = True,
         cycle: int = 3,
+        constraint_weight: float = 5.0,
         n_threads: int = 4,
         batch_size: int = None,
         parallelize: bool = False,
@@ -82,6 +160,7 @@ class RosettaEnergyBlackBox(AbstractBlackBox):
         self.relax = relax
         self.pack = pack
         self.cycle = cycle
+        self.constraint_weight = constraint_weight
         self.n_threads = n_threads
 
         try:
@@ -100,6 +179,7 @@ class RosettaEnergyBlackBox(AbstractBlackBox):
                 relax=self.relax,
                 pack=self.pack,
                 cycle=self.cycle,
+                constraint_weight=self.constraint_weight,
                 n_threads=self.n_threads,
             )
             self.inner_function = opt_in_wrapper(inner_function)
@@ -126,13 +206,16 @@ class RosettaEnergyBlackBox(AbstractBlackBox):
         Returns
         -------
         y : np.ndarray
-            The stability(/REUs) of the mutant(s) in x.
+            The stability(DDGs/REUs/DREUs) of the mutant(s) in x.
         """
         return self.inner_function(x, context)
 
     # A static method that gives you access to the information.
     @staticmethod
     def get_black_box_info() -> BlackBoxInformation:
+        """
+        Returns the black box information for Rosetta.
+        """
         return rosetta_energy_information
 
 
@@ -151,6 +234,7 @@ class RosettaEnergyProblemFactory(AbstractProblemFactory):
         relax: bool = True,
         pack: bool = True,
         cycle: int = 3,
+        constraint_weight: int | float = 5,
         n_threads: int = 4,
         batch_size: int = None,
         parallelize: bool = False,
@@ -158,6 +242,67 @@ class RosettaEnergyProblemFactory(AbstractProblemFactory):
         evaluation_budget: int = float("inf"),
         force_isolation: bool = False,
     ) -> Problem:
+        """
+        Creates a Rosetta black box instance, alongside initial
+        observations.
+
+        Parameters
+        ----------
+        wildtype_pdb_path : Path
+            The path to the wildtype PDB file(s), by default None.
+            NOTE: currently only for single PDB files implemented -- List of Paths not yet supported!
+        score_function : str, optional
+            Which Rosetta score function to use. Options are [ref2015 , default , centroid , fa, ref2015_cart, franklin2019].
+            The default function references ref2015.
+        seed : int, optional
+            Overwrite Rosetta random seed with own integer, uses mt19937 RT reference (as per Rosetta default).
+        unit : str, optional
+            Output unit of black-box. Default is DDG, which is scaled difference between variant and wild-type.
+            Alternatives are:
+                REU -- raw energy function value,
+                DREU -- energy unit delta to wild-type.
+        conversion_factor : float, optional
+            Scaling factor applied when computing DDGs (DDG=deltaREU/conversion_factor). Only applies to DDG conversion.
+            Default is '2.9' following:
+                Park, Hahnbeom, et al.
+                "Simultaneous optimization of biomolecular energy functions on features from small molecules and macromolecules."
+                Journal of chemical theory and computation 12.12 (2016): 6201-6212.
+                DOI: https://doi.org/10.1021/acs.jctc.6b00819
+        clean : bool, optional
+            Flag whether to apply PDB cleaning routine prior to pose loading of reference.
+            Default is True.
+        relax: bool, optional
+            Flag whether to apply relaxation protocol. Default enabled.
+            NOTE: disable if fast compute required
+        pack: bool, optional
+            Flag whether to apply packing protocol. Default enabled.
+            NOTE: disable if fast compute required
+        cycle: int, optional
+            Number of relaxation cycles applied.
+        constraint_weight: int, optional
+            Constraint on scoring function for atom-pair, angles, coordinates.
+        batch_size : int, optional
+            The batch size for parallel evaluation, by default None.
+        parallelize : bool, optional
+            Flag indicating whether to parallelize evaluation, by default False.
+        num_workers : int, optional
+            The number of workers for parallel evaluation, by default None.
+        evaluation_budget : int, optional
+            The evaluation budget, by default float("inf").
+
+        Returns
+        -------
+        Problem : poli.core.problem.Problem
+            The poli Problem instance, containing f , x0 , y0
+            where
+            f : RosettaEnergyBlackBox
+                The Rosetta black box instance.
+            x0 : np.ndarray
+                The initial observations (i.e. the wildtype as sequences
+                of amino acids).
+            y0 : np.ndarray
+                The initial observations (i.e. the stability of the wildtype).
+        """
         # Creating your black box function
         f = RosettaEnergyBlackBox(
             wildtype_pdb_path=wildtype_pdb_path,
@@ -169,6 +314,7 @@ class RosettaEnergyProblemFactory(AbstractProblemFactory):
             relax=relax,
             pack=pack,
             cycle=cycle,
+            constraint_weight=constraint_weight,
             n_threads=n_threads,
             batch_size=batch_size,
             parallelize=parallelize,
