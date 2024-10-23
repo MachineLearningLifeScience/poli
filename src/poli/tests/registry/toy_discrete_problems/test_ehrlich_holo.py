d390d9c3977dedf5fde192527ebef5de686bd085
Miguel González Duque <miguelgondu@gmail.com>
2024-10-23 14:52:23 +0200

    Comments the old test again

--- src/poli/tests/registry/toy_discrete_problems/test_ehrlich_holo.py
+++ src/poli/tests/registry/toy_discrete_problems/test_ehrlich_holo.py
@@ -32,22 +32,38 @@ def test_ehrlich_holo_builds_and_queries():
     print(black_box(x_final))
 
 
-@pytest.mark.poli__ehrlich_holo
-def test_ehrlich_holo_works_on_isolation():
-    from poli.objective_repository import EhrlichHoloBlackBox
+"""
+This test is currently not working on CI. The reason
+seems to be something related to the networking of
+the CI environment, and the fact that we are querying
+properties/methods of the underlying ExternalIsolatedFunction.
 
-    black_box = EhrlichHoloBlackBox(
-        sequence_length=10,
-        motif_length=3,
-        n_motifs=2,
-        epistasis_factor=0.0,
-        force_isolation=True,
-    )
-    x0 = black_box.initial_solution(100)
-    print(black_box(x0))
+These issues will be addressed by the new isolation interface,
+and if the user installs this black box with
+`pip install poli[ehrlich]` they won't face these issues.
 
-    x_final = black_box.optimal_solution()
-    print(black_box(x_final))
+The error we currently get is the following:
+multiprocessing.context.AuthenticationError: digest sent was rejected
+
+Testing it locally seems to work well.
+TODO: install in Colab and test.
+"""
+# @pytest.mark.poli__ehrlich_holo
+# def test_ehrlich_holo_works_on_isolation():
+#     from poli.objective_repository import EhrlichHoloBlackBox
+
+#     black_box = EhrlichHoloBlackBox(
+#         sequence_length=10,
+#         motif_length=3,
+#         n_motifs=2,
+#         epistasis_factor=0.0,
+#         force_isolation=True,
+#     )
+#     x0 = black_box.initial_solution(100)
+#     print(black_box(x0))
+
+#     x_final = black_box.optimal_solution()
+#     print(black_box(x_final))
 
 
 @pytest.mark.poli__ehrlich_holo
@@ -76,7 +92,3 @@ def test_ehrlich_seed_determinism():
 
     assert (black_box(x0) == black_box_2(x0_2)).all()
     assert (x0 == x0_2).all()
-
-
-if __name__ == "__main__":
-    test_ehrlich_holo_works_on_isolation()
