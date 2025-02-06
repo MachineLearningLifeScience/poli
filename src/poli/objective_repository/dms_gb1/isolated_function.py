4703c041e5a71d30233d86b2c047ad5421cbe1b7
Richard Michael <r.michael@posteo.net>
2025-02-06 14:56:05 +0100

    add GB1 black box and isolation

--- src/poli/objective_repository/dms_gb1/isolated_function.py
+++ src/poli/objective_repository/dms_gb1/isolated_function.py
@@ -39,10 +39,6 @@ class DMSGB1IsolatedLogic(AbstractIsolatedFunction):
 
     Parameters
     ----------
-    penalize_unfeasible_with: float, optional
-        The value to return when the input is unfeasible, by default None, which means that we raise an error when
-        an unfeasible sequence (e.g. one with a length different
-        from the wildtypes) is passed.
     alphabet : List[str], optional
         The alphabet for the problem, by default we use
         the amino acid list provided in poli.core.util.proteins.defaults.
@@ -64,7 +60,6 @@ class DMSGB1IsolatedLogic(AbstractIsolatedFunction):
 
     def __init__(
         self,
-        penalize_unfeasible_with: float | None = np.inf,
         experiment_id: str = None,
     ):
         """
@@ -72,130 +67,41 @@ class DMSGB1IsolatedLogic(AbstractIsolatedFunction):
 
         Parameters:
         -----------
-        penalize_unfeasible_with: float, optional
-            The value to return when the input is unfeasible, by default None, which means that we raise an error when
-            an unfeasible sequence (e.g. one with a length different
-            from the wildtypes) is passed.
+
         experiment_id : str, optional
             The experiment ID, by default None.
-        tmp_folder : Path, optional
-            The temporary folder path, by default None.
 
         Notes:
         ------
         - If experiment_id is not provided, it is generated using the current timestamp and a random UUID.
         """
-        assert wildtype_pdb_path is not None, (
-            "Missing required argument wildtype_pdb_file. "
-            "Did you forget to pass it to create and into the black box?"
-        )
-
-        if isinstance(wildtype_pdb_path, str):
-            wildtype_pdb_path = Path(wildtype_pdb_path.strip())
-
-        if isinstance(wildtype_pdb_path, Path):
-            wildtype_pdb_path = [wildtype_pdb_path]
-
-        if isinstance(wildtype_pdb_path, list):
-            if isinstance(wildtype_pdb_path[0], str):
-                # Assuming that wildtype_pdb_path is a list of strings
-                wildtype_pdb_path = [Path(x.strip()) for x in wildtype_pdb_path]
-            elif isinstance(wildtype_pdb_path[0], Path):
-                pass
-
-        # By this point, we can make sure the wildtype_pdb_path
-        # is a list of Path objects.
-        assert all([x.exists() for x in wildtype_pdb_path]), (
-            "One of the wildtype PDBs does not exist. "
-            "Please check the path and try again."
-        )
-
-        self.wildtype_pdb_paths = wildtype_pdb_path
-
-        self.wildtype_residue_strings = [
-            "".join(amino_acids) for amino_acids in self.wildtype_amino_acids
-        ]
-
-        # Validating the chains to keep
-        if isinstance(chains_to_keep, type(None)):
-            # Defaulting to always keeping chain A.
-            chains_to_keep = ["A"] * len(self.wildtype_pdb_paths)
-
-        if isinstance(chains_to_keep, str):
-            chains_to_keep = [chains_to_keep] * len(self.wildtype_pdb_paths)
-
-        if isinstance(chains_to_keep, list):
-            assert len(chains_to_keep) == len(self.wildtype_pdb_paths), (
-                "The number of chains to keep must be the same as the number of wildtypes."
-                " You can specify a single chain to keep for all wildtypes, or a list of chains."
-            )
-
-        self.penalize_unfeasible_with = penalize_unfeasible_with
 
         if experiment_id is None:
             experiment_id = f"{int(time())}_{str(uuid4())[:8]}"
         self.experiment_id = experiment_id
 
-        x0_pre_array = []
-        for clean_wildtype_pdb_file in self.clean_wildtype_pdb_files:
-            # Loads up the wildtype pdb files as strings
-            wildtype_string = self.parse_pdb_as_residue_strings(clean_wildtype_pdb_file)
-            x0_pre_array.append(list(wildtype_string))
+        # see [2] for reference:
+        # "we investigated the fitness landscape of all variants
+        # (204 = 160,000) at four amino acid sites (V39, D40, G41 and V54)
+        # in an epistatic region of protein G domain B1"
+        self.wt = "VDGV"
+        self.positions = [39, 40, 41, 54]  # included for completeness
 
+        self.dms_df = self._load_dms_data()
         # Padding all of them to the longest sequence
-        max_len = max([len(x) for x in x0_pre_array])
-        x0_pre_array = [x + [""] * (max_len - len(x)) for x in x0_pre_array]
+        max_len = len(self.wt)
 
-        self.x0 = np.array(x0_pre_array)
-
-    def _clean_wildtype_pdb_files(self):
-        """
-        This function cleans the wildtype pdb files
-        stored in self.wildtype_pdb_paths, using
-        cached results if they exist.
-        """
-
-        # We make sure the PDBs are cleaned.
-        # TODO: add chain to the name of the file
-        for wildtype_pdb_path, chain_to_keep in zip(
-            self.wildtype_pdb_paths, self.chains_to_keep
-        ):
-            if not (
-                self.working_dir
-                / "raw"
-                / f"{wildtype_pdb_path.stem}_{chain_to_keep}.pdb"
-            ).exists():
-                self.rasp_interface.raw_pdb_to_unique_chain(
-                    wildtype_pdb_path, chain=chain_to_keep
-                )
-
-            if not (
-                self.working_dir
-                / "cleaned"
-                / f"{wildtype_pdb_path.stem}_{chain_to_keep}_clean.pdb"
-            ).exists():
-                self.rasp_interface.unique_chain_to_clean_pdb(wildtype_pdb_path)
-
-            if not (
-                self.working_dir
-                / "parsed"
-                / f"{wildtype_pdb_path.stem}_{chain_to_keep}_clean_coordinate_features.npz"
-            ).exists():
-                self.rasp_interface.cleaned_to_parsed_pdb(wildtype_pdb_path)
-
-        self.clean_wildtype_pdb_files = [
-            self.working_dir
-            / "cleaned"
-            / f"{wildtype_pdb_path.stem}_query_protein_uniquechain_clean.pdb"
-            for wildtype_pdb_path in self.wildtype_pdb_paths
-        ]
+        self.x0 = np.array(list(self.dms_df.iloc[self.wt].value))
 
     def _load_dms_data(self) -> pd.DataFrame:
-        pd.read_csv(THIS_DIR / "assets" / "fitnes.csv")
+        pd.read_csv(THIS_DIR / "assets" / "fitness.csv")
+
+    def _return_dms_val(self, x) -> float:
+        return self.dms_df.iloc[x].value
 
     def __call__(self, x, context=None):
         """
-        Computes the stability of the mutant(s) in x.
+        Computes the fitness of the variant(s) in x.
 
         Parameters
         ----------
@@ -207,43 +113,35 @@ class DMSGB1IsolatedLogic(AbstractIsolatedFunction):
         Returns
         -------
         y : np.ndarray
-            The stability of the mutant(s) in x.
+            The fitness of the mutant(s) in x.
 
         Notes
         -----
         - x is a np.array[str] of shape [b, L], where L is the length
           of the longest sequence in the batch, and b is the batch size.
-          We process it by concantenating the array into a single string,
-          where we assume the padding to be an empty string (if there was any).
-          Each of these x_i's will be matched to the wildtype in
-          self.wildtype_residue_strings with the lowest Hamming distance.
-        """
-        # We need to find the closest wildtype to each of the
-        # sequences in x. For this, we need to compute the
-        # Hamming distance between each of the sequences in x
-        # and each of the wildtypes in self.wildtype_residue_strings.
 
-        # closest_wildtypes will be a dictionary
-        # of the form {wildtype_path: List[str] of mutations}
-        # closest_wildtypes = defaultdict(list)
-        # mutant_residue_strings = []
-        # mutant_residue_to_hamming_distances = dict()
+        Throws
+        -----
+        - ValueError exception if input is invalid.
+        """
         results = []
         for x_i in x:
             # Assuming x_i is an array of strings
+            if len(x_i) != 4:
+                raise ValueError("Inputs of L=4 expected!")
             mutant_residue_string = "".join(x_i)
-            result = self._compute_mutant_residue_string_ddg(mutant_residue_string)
+            result = self._return_dms_val(mutant_residue_string)
             results.append(result)
 
-        return -np.array(results).reshape(-1, 1)
+        return np.array(results).reshape(-1, 1)
 
 
 if __name__ == "__main__":
     from poli.core.registry import register_isolated_function
 
     register_isolated_function(
-        RaspIsolatedLogic,
-        name="rasp__isolated",
-        conda_environment_name="poli__rasp",
+        DMSGB1IsolatedLogic,
+        name="dmsgb1__isolated",
+        conda_environment_name="poli__dms",
         force=True,
     )
--- src/poli/objective_repository/dms_gb1/register.py
+++ src/poli/objective_repository/dms_gb1/register.py
@@ -1,9 +1,9 @@
 """
 This module implements the DMS GB1 black-box,
-using the open-available data from [1].
+using the data available from [1].
 
 The black-box is a lookup of the combinatorically complete mutation landscape.
-If the proposed sequence exists the associated value is returned, else a nan/inf value is returned.
+The proposed sequence has to exist and the associated value is returned.
 
 The dataset reference is [2].
 
@@ -15,6 +15,9 @@ Lin Chen, Zehong Zhang, Zhenghao Li, Rui Li, Ruifeng Huo, Lifan Chen, Dingyan Wa
 Cell Systems,
 Volume 14, Issue 8, 2023,
 ISSN 2405-4712; doi: https://doi.org/10.1016/j.cels.2023.07.003.]
+
+All rights of the data remain with the authors of the respective publications ([1], [2])
+under the licenses of the provided resources.
 
 """
 
@@ -38,6 +41,10 @@ class DMSGB1BlackBox(AbstractBlackBox):
 
     Parameters
     ----------
+    negative: bool, optional
+        Apply sign-flip on observations for a minimizing black-box.
+    experiment_id : str, optional
+        The experiment ID, by default None.
     batch_size : int, optional
         The batch size for parallel evaluation, by default None.
     parallelize : bool, optional
@@ -57,46 +64,28 @@ class DMSGB1BlackBox(AbstractBlackBox):
 
     Notes
     -----
-    - TODO
+    ...
     """
 
     def __init__(
         self,
+        negative: bool = False,
         experiment_id: str = None,
         batch_size: int = None,
         parallelize: bool = False,
         num_workers: int = None,
         evaluation_budget: int = float("inf"),
-        penalize_unfeasible_with: float | None = float("inf"),
         force_isolation: bool = False,
     ):
         """
-        Initialize the RaSP Register object.
+        Initialize the DMSGB1 Register object.
 
         Parameters:
         -----------
-        wildtype_pdb_path : Union[Path, List[Path]]
-            The path(s) to the wildtype PDB file(s).
-        additive : bool, optional
-            Whether we treat multiple mutations as additive, by default False.
-            If you are interested in running this black box with multiple
-            mutations, you should set this to True. Otherwise, it will
-            raise an error if you pass a sequence with more than one mutation.
-        chains_to_keep : List[str], optional
-            The chains to keep in the PDB file(s), by default we
-            keep the chain "A" for all pdbs passed.
-        penalize_unfeasible_with : float | None, optional
-            The value to penalize unfeasible solutions with, by default None, which means we raise an error when
-            an unfeasible sequence (e.g. a sequence with a length
-            different from the wildtypes) is passed.
-        device: str, optional
-            The device to load the models on, by default None, which
-            means that RaSP decides to run on either CUDA or
-            CPU, depending on the availability of CUDA.
+        negative: bool, optional
+            Apply sign flip to observations, ie. for minimizing black-box.
         experiment_id : str, optional
             The experiment ID, by default None.
-        tmp_folder : Path, optional
-            The temporary folder path, by default None.
         batch_size : int, optional
             The batch size for parallel evaluation, by default None.
         parallelize : bool, optional
@@ -108,23 +97,15 @@ class DMSGB1BlackBox(AbstractBlackBox):
 
         Raises:
         -------
-        AssertionError
-            If wildtype_pdb_path is not provided.
+        ...
 
         Notes:
         ------
-        - The wildtype_pdb_path can be a single Path object or a list of Path objects.
-        - If chains_to_keep is not provided, it defaults to keeping chain A for all wildtypes.
-        - If experiment_id is not provided, it is generated using the current timestamp and a random UUID.
-        - If tmp_folder is not provided, it defaults to the default temporary path.
+        ...
         """
-        assert wildtype_pdb_path is not None, (
-            "Missing required argument wildtype_pdb_file. "
-            "Did you forget to pass it to create and into the black box?"
-        )
         if parallelize:
             print(
-                "poli 🧪: RaspBlackBox parallelization is handled by the isolated logic. Disabling it."
+                "poli 🧪: BlackBox parallelization is handled by the isolated logic. Disabling it."
             )
             parallelize = False
         super().__init__(
@@ -133,32 +114,21 @@ class DMSGB1BlackBox(AbstractBlackBox):
             num_workers=num_workers,
             evaluation_budget=evaluation_budget,
         )
+        self.negative = negative
         self.force_isolation = force_isolation
-        self.wildtype_pdb_path = wildtype_pdb_path
-        self.chains_to_keep = chains_to_keep
         self.experiment_id = experiment_id
-        self.tmp_folder = tmp_folder
-        self.additive = additive
-        self.penalize_unfeasible_with = penalize_unfeasible_with
-        self.device = device
         self.inner_function = get_inner_function(
-            isolated_function_name="rasp__isolated",
-            class_name="RaspIsolatedLogic",
-            module_to_import="poli.objective_repository.rasp.isolated_function",
+            isolated_function_name="dms_gb1__isolated",
+            class_name="DMSGB1IsolatedLogic",
+            module_to_import="poli.objective_repository.dms_gb1.isolated_function",
             force_isolation=self.force_isolation,
-            wildtype_pdb_path=self.wildtype_pdb_path,
-            additive=self.additive,
-            chains_to_keep=self.chains_to_keep,
-            penalize_unfeasible_with=self.penalize_unfeasible_with,
             experiment_id=self.experiment_id,
-            tmp_folder=self.tmp_folder,
-            device=self.device,
         )
         self.x0 = self.inner_function.x0
 
     def _black_box(self, x, context=None):
         """
-        Computes the stability of the mutant(s) in x.
+        Computes the fitness of the mutant(s) in x.
 
         Parameters
         ----------
@@ -170,27 +140,31 @@ class DMSGB1BlackBox(AbstractBlackBox):
         Returns
         -------
         y : np.ndarray
-            The stability of the mutant(s) in x.
+            The fitness of the mutant(s) in x.
 
         Notes
         -----
         - x is a np.array[str] of shape [b, L], where L is the length
-          of the longest sequence in the batch, and b is the batch size.
+          of the chain of residues, and b is the batch size.
           We process it by concantenating the array into a single string,
           where we assume the padding to be an empty string (if there was any).
           Each of these x_i's will be matched to the wildtype in self.  wildtype_residue_strings with the lowest Hamming distance.
+        - negative flag inverts sign of observations, ie. for minimizing the black-box
         """
-        return self.inner_function(x, context=context)
+        y = self.inner_function(x, context=context)
+        if self.negative:
+            y = -y
+        return y
 
     def get_black_box_info(self) -> BlackBoxInformation:
         """
-        Returns the black box information for RaSP.
+        Returns the black box information for DMSGB1.
         """
-        is_aligned = False if len(self.wildtype_pdb_path) > 1 else True
-        is_fixed_length = False if len(self.wildtype_pdb_path) > 1 else True
+        is_aligned = True
+        is_fixed_length = True
         max_sequence_length = max([len("".join(x)) for x in self.x0])
         return BlackBoxInformation(
-            name="rasp",
+            name="dms_gb1",
             max_sequence_length=max_sequence_length,
             aligned=is_aligned,
             fixed_length=is_fixed_length,
@@ -203,16 +177,11 @@ class DMSGB1BlackBox(AbstractBlackBox):
         )
 
 
-class RaspProblemFactory(AbstractProblemFactory):
+class DMSGB1ProblemFactory(AbstractProblemFactory):
     def create(
         self,
-        wildtype_pdb_path: Union[Path, List[Path]],
-        additive: bool = False,
-        chains_to_keep: List[str] = None,
-        penalize_unfeasible_with: float | None = None,
-        device: str | None = None,
+        negative: bool = False,
         experiment_id: str = None,
-        tmp_folder: Path = None,
         seed: int = None,
         batch_size: int = None,
         parallelize: bool = False,
@@ -221,33 +190,15 @@ class RaspProblemFactory(AbstractProblemFactory):
         force_isolation: bool = False,
     ) -> Problem:
         """
-        Creates a RaSP black box instance, alongside initial
+        Creates a DMS GB1 black box instance, alongside initial
         observations.
 
         Parameters
         ----------
-        wildtype_pdb_path : Union[Path, List[Path]]
-            The path(s) to the wildtype PDB file(s).
-        additive: bool, optional
-            Whether we treat multiple mutations as additive, by default False.
-            If you are interested in running this black box with multiple
-            mutations, you should set this to True. Otherwise, it will
-            raise an error if you pass a sequence with more than one mutation.
-        chains_to_keep : List[str], optional
-            The chains to keep in the PDB file(s), by default we
-            keep the chain "A" for all pdbs passed.
-        penalize_unfeasible_with : float | None, optional
-            The value to penalize unfeasible solutions with, by default None,
-            which means we raise an error when an unfeasible sequence (e.g.
-            a sequence with a length different from the wildtypes) is passed.
-        device: str, optional
-            The device to load the models on, by default None,
-            which means that RaSP decides to run on either CUDA or
-            CPU, depending on the availability of CUDA.
+        negative: bool, optional
+            Invert signs of observations, by default False.
         experiment_id : str, optional
             The experiment ID, by default None.
-        tmp_folder : Path, optional
-            The temporary folder path, by default None.
         seed : int, optional
             The seed value for random number generation, by default None.
         batch_size : int, optional
@@ -261,10 +212,10 @@ class RaspProblemFactory(AbstractProblemFactory):
 
         Returns
         -------
-        f : RaspBlackBox
-            The RaSP black box instance.
+        f : DMSGB1BlackBox
+            The DMS black box instance.
         x0 : np.ndarray
-            The initial observations (i.e. the wildtypes as sequences
+            The initial observations (i.e. the wildtype as a sequence
             of amino acids).
         y0 : np.ndarray
             The initial observations (i.e. the stability of the wildtypes).
@@ -272,35 +223,9 @@ class RaspProblemFactory(AbstractProblemFactory):
         if seed is not None:
             seed_python_numpy_and_torch(seed)
 
-        if wildtype_pdb_path is None:
-            raise ValueError(
-                "Missing required argument wildtype_pdb_path. "
-                "Did you forget to pass it to create()?"
-            )
-
-        if isinstance(wildtype_pdb_path, str):
-            wildtype_pdb_path = [Path(wildtype_pdb_path.strip())]
-        elif isinstance(wildtype_pdb_path, Path):
-            wildtype_pdb_path = [wildtype_pdb_path]
-        elif isinstance(wildtype_pdb_path, list):
-            if isinstance(wildtype_pdb_path[0], str):
-                wildtype_pdb_path = [Path(x.strip()) for x in wildtype_pdb_path]
-            elif isinstance(wildtype_pdb_path[0], Path):
-                pass
-        else:
-            raise ValueError(
-                "Invalid type for wildtype_pdb_path. "
-                "It must be a string, a Path, or a list of strings or Paths."
-            )
-
-        f = RaspBlackBox(
-            wildtype_pdb_path=wildtype_pdb_path,
-            additive=additive,
-            chains_to_keep=chains_to_keep,
-            penalize_unfeasible_with=penalize_unfeasible_with,
-            device=device,
+        f = DMSGB1BlackBox(
+            negative=negative,
             experiment_id=experiment_id,
-            tmp_folder=tmp_folder,
             batch_size=batch_size,
             parallelize=parallelize,
             num_workers=num_workers,
